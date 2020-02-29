a049c782d10edd7778df4e4c53824783ddc79855
Kaustubh Khandagale <kaustubh.khandagale@tcs.com>
2020-02-29 19:26:55 +0530

    update project name in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/nasa-api'));
+app.use(express.static(__dirname + '/dist/NASA-API'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/nasa-api/index.html'));
+res.sendFile(path.join(__dirname+'/dist/NASA-API/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
